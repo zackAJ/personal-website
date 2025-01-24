0e378bafc767108833529cde35e5634452cf1890
zackaj <aktouf98zakaria@gmail.com>
2025-01-25 00:39:38 +0100

    update title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "ZackAJ blog",
+  DESCRIPTION: "My personel website where I share my work, thoughts and passion",
+  AUTHOR: "Zakaria AKTOUF",
 }
 
 // Work Page
